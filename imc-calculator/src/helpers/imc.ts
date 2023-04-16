6a18974fb904b463c66cb0eee329e6a55d7390c5
CamiNunes <cvn.camila@gmail.com>
2023-04-16 16:15:07 -0300

    Criando os cards de cada status do IMC;

--- imc-calculator/src/helpers/imc.ts
+++ imc-calculator/src/helpers/imc.ts
@@ -7,9 +7,9 @@ export type Level ={
 }
 
 export const levels: Level[] = [
-  { title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.5]}  ,
   { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
-  { title: 'Obesidade', color: '#E2B036', icon: 'down', imc: [25, 30]},
+  { title: 'Magreza', color: '#E2B036', icon: 'down', imc: [0, 18.5]}  ,
+  { title: 'Obesidade', color: '#d16038', icon: 'down', imc: [25, 30]},
   { title: 'Sobrepeso', color: '#C3423F', icon: 'down', imc: [30.1, 99]} 
 ];
 
